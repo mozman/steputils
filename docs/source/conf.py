6fa6c82b593bcc7d1095b50a059a4bc323f2cec1
mozman <me@mozman.at>
2022-03-29 06:27:45 +0200

    update Sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,13 +9,16 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+import sys
+import os
+sys.path.insert(0, os.path.abspath("../../src"))
 import steputils
 
 # -- Project information -----------------------------------------------------
 
 
 project = 'STEPutils'
-copyright = '2019-2020, Manfred Moitzi'
+copyright = '2019-2022, Manfred Moitzi'
 author = steputils.__author__
 
 # The short X.Y version.
